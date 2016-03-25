4c9784edd1bed819c3be0e35f5a1bf79c0df0540
smlc <siesa.marlon@hotmail.fr>
2016-03-25 17:26:09 +0100

    Modif module

--- Application-ServicesVelib/ClientJersey/src/main/java/domain/ApiJCDecaux.java
+++ Application-ServicesVelib/ClientJersey/src/main/java/domain/ApiJCDecaux.java
@@ -11,7 +11,6 @@ import javax.json.*;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.client.Invocation;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
 
