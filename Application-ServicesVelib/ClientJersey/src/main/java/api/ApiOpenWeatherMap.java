f01bec692ed08ccd1088c1a6ec31d8e37d00ee2a
lovajenny <lovajenny.andriana@gmail.com>
2016-03-30 18:36:34 +0200

    #5 Modif Api OpenWeatherMap

--- Application-ServicesVelib/ClientJersey/src/main/java/api/ApiOpenWeatherMap.java
+++ Application-ServicesVelib/ClientJersey/src/main/java/api/ApiOpenWeatherMap.java
@@ -16,13 +16,14 @@ public class ApiOpenWeatherMap {
     WebTarget wt;
 
     private static String apiKey = "fdace7509c0b11ca8b680e39e39d916f";
-    private static String UriApi = "http://api.openweathermap.org/data/2.5/weather?appid="+apiKey+"&mode=xml";
+    private static String UriApi = "http://api.openweathermap.org/data/2.5/weather?appid="+apiKey;
 
     public ApiOpenWeatherMap () {
 
         wt = ClientBuilder.newClient().target(UriApi);
 
-        Document docXML = wt.queryParam("q", "toulouse").request(MediaType.APPLICATION_XML).get(Document.class);
+        Document docXML = wt.queryParam("mode", "xml").queryParam("q", "toulouse")
+                .request(MediaType.APPLICATION_XML).get(Document.class);
 
     }
 
