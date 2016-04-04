5b21d2dd7ab55479e61bb72dabf63b66f3ef8208
smlc <siesa.marlon@hotmail.fr>
2016-04-04 13:26:03 +0200

    #10 interface mediateur

--- Application-ServicesVelib/ClientJersey/src/main/java/services/Mediateur.java
+++ Application-ServicesVelib/ClientJersey/src/main/java/services/Mediateur.java
@@ -3,5 +3,5 @@ package services;
 /**
  * Created by mars on 03/04/16.
  */
-public class Mediateur {
+public class Mediateur implements MediateurService {
 }
---
+++ Application-ServicesVelib/ClientJersey/src/main/java/services/MediateurService.java
@@ -0,0 +1,8 @@
+package services;
+
+/**
+ * Created by mars on 04/04/16.
+ */
+public interface MediateurService {
+
+}
