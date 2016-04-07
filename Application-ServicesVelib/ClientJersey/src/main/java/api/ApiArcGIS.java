2ba51b7f1173eceadc27a032ce0fb2573709d12a
lovajenny <lovajenny.andriana@gmail.com>
2016-04-07 11:44:28 +0200

    #5 Mise a jour de l'api ArcGIS

--- Application-ServicesVelib/ClientJersey/src/main/java/api/ApiArcGIS.java
+++ Application-ServicesVelib/ClientJersey/src/main/java/api/ApiArcGIS.java
@@ -1,7 +1,6 @@
 package api;
 
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import domain.Coordonne;
 import domain.Station;
 
@@ -80,11 +79,11 @@ public class ApiArcGIS {
         return 0.;
     }
 
-    public void getLengths (Coordonne adressClient, List<Station> stations) {
+    public Map<Station, Double> getLengths (Coordonne adressClient, List<Station> stations) {
 
         MultivaluedMap<String, String> formData = new MultivaluedHashMap<String, String>();
         List<Station> candidateStations = new ArrayList<>();
-        List<Double> distances = new ArrayList<>();
+        Map<Station, Double> threeStations = new HashMap<>();
 
         String jsonRequest = "[";
         for (int i=0; i<stations.size(); i++) {
@@ -109,11 +108,42 @@ public class ApiArcGIS {
         formData.putSingle("polylines", polylines.toString());
         formData.putSingle("calculationType", "preserveShape");
 
-        JsonObject response = wt.request(MediaType.APPLICATION_JSON).post(Entity.form(formData), JsonObject.class);
-        response.getJsonArray("lengths");
+        JsonArray response = wt.request(MediaType.APPLICATION_JSON).post(Entity.form(formData), JsonObject.class)
+                .getJsonArray("lengths");
 
-        System.out.println(response);
-        System.out.println(response.getJsonArray("lengths").get(0));
+        threeStations.putAll(researchThreeLengthsMin(response, candidateStations));
 
+        return threeStations;
+    }
+
+    private Map<Station, Double> researchThreeLengthsMin (JsonArray response, List<Station> candidateStations) {
+
+        // Liste contenant toutes les stations
+            ArrayList<Double> stationList = new ArrayList<Double>();
+        // Liste des trois stations avec leur distance
+            Map<Station, Double> threeStations = new HashMap<>();
+        // Tampons contenant l'indice et la distance minimale
+            Double distanceMin;
+            int indexMin = 0;
+
+        // Récupération de toutes les stations
+        for (int i=0; i<response.size(); i++) {
+            stationList.add(Double.parseDouble(response.get(i).toString()));
+        }
+
+        for (int j=0; j<3; j++) {
+            distanceMin = stationList.get(0);
+            for (int i=1; i<stationList.size(); i++) {
+                if (stationList.get(i) < distanceMin) {
+                    distanceMin = stationList.get(i);
+                    indexMin = i;
+                }
+            }
+            threeStations.put(candidateStations.get(indexMin), distanceMin);
+            stationList.remove(indexMin);
+            candidateStations.remove(indexMin);
+        }
+
+        return threeStations;
     }
 }
--- Application-ServicesVelib/ClientJersey/src/test/java/groupId/ApiArcGISTest.java
+++ Application-ServicesVelib/ClientJersey/src/test/java/groupId/ApiArcGISTest.java
@@ -66,29 +66,37 @@ public class ApiArcGISTest {
         Map<Object, Object> position4 = new HashMap();
         Map<Object, Object> position5 = new HashMap();
         Map<Object, Object> position6 = new HashMap();
+        Map<Object, Object> threeStations = new HashMap();
+        Map<Object, Object> threeStationsResultat = new HashMap();
 
         position.put("lat", "43.608951960496405");
         position.put("lng", "1.441003598726198");
+        s1.setName("s1");
         s1.setPosition(position);
         s1.setAvailable_bikes(6);
         position2.put("lat", "43.59723540303583");
         position2.put("lng", "1.45907112459247");
+        s2.setName("s2");
         s2.setPosition(position2);
         s2.setAvailable_bikes(0);
         position3.put("lat", "43.571433064051334");
         position3.put("lng", "1.46278065695466");
+        s3.setName("s3");
         s3.setPosition(position3);
         s3.setAvailable_bikes(9);
         position4.put("lat", "43.597074846780274");
         position4.put("lng", "1.452605307400692");
+        s4.setName("s4");
         s4.setPosition(position4);
         s4.setAvailable_bikes(5);
         position5.put("lat", "43.60221990815785");
         position5.put("lng", "1.45195665819857");
+        s5.setName("s5");
         s5.setPosition(position5);
         s5.setAvailable_bikes(4);
         position6.put("lat", "43.59729443895585");
         position6.put("lng", "1.403872948950036");
+        s6.setName("s6");
         s6.setPosition(position6);
         s6.setAvailable_bikes(4);
 
@@ -99,9 +107,13 @@ public class ApiArcGISTest {
         stations.add(s5);
         stations.add(s6);
 
+        threeStations.put(s1, 0.005301113355925021);
+        threeStations.put(s5, 0.014995485792134916);
+        threeStations.put(s4, 0.017303176346981187);
+
         //When
-        api.getLengths (adressClient, stations);
+        threeStationsResultat.putAll(api.getLengths (adressClient, stations));
 
-
+        assertEquals(threeStations, threeStationsResultat);
     }
 }
